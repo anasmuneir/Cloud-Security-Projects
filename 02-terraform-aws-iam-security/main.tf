6b61da86206e840cb9d10c6e831de26bfe22641d
Anas Muneir <anasmuneir@gmail.com>
2026-06-09 02:56:41 +0400

    Implement IAM user, group, and membership with Terraform

--- 02-terraform-aws-iam-security/main.tf
+++ 02-terraform-aws-iam-security/main.tf
@@ -15,3 +15,15 @@ provider "aws" {
 resource "aws_iam_group" "security_team" {
   name = "security_team"
 }
+
+resource "aws_iam_user" "security_analyst" {
+  name = "security_analyst"
+}
+
+resource "aws_iam_user_group_membership" "security_team_membership" {
+  user = aws_iam_user.security_analyst.name
+
+  groups = [
+    aws_iam_group.security_team.name
+  ]
+}
