363684cd73c0148c99a1ce5d01062e71956eebe8
Anas Muneir <anasmuneir@gmail.com>
2026-06-12 00:18:49 +0400

    Implement S3 versioning for data protection

--- 03-terraform-aws-s3-security/main.tf
+++ 03-terraform-aws-s3-security/main.tf
@@ -13,6 +13,15 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_s3_bucket" "secure-bucket" {
+resource "aws_s3_bucket" "secure_bucket" {
   bucket = "anas-cloud-security-s3-demo"
 }
+
+resource "aws_s3_bucket_versioning" "secure_bucket_versioning" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
