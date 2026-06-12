f9ab83ce4b2dc076d280242309c5faa5fd5b3831
Anas Muneir <anasmuneir@gmail.com>
2026-06-13 03:54:23 +0400

    Implement VPC and subnet network segmentation

--- 04-terraform-aws-secure-vpc-ec2/main.tf
+++ 04-terraform-aws-secure-vpc-ec2/main.tf
@@ -1,13 +1,41 @@
+# Configure the AWS provider and enforce a consister provider version for reproducible infrastructure deployment
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
 # Create a dedicated VPC to isolate project resources from the default AWS network
 resource "aws_vpc" "secure_vpc" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-        name = "secure_vpc"
-    }
-} 
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "secure-vpc"
+  }
+}
 
-#Public subnet for internet-facing resources
+# Public subnet for internet-facing resources
 resource "aws_subnet" "public_subnet" {
-    vpc_id = aws_vpc.secure_vpc.id
-    cidr_block = 10.0.1.0/24
+  vpc_id     = aws_vpc.secure_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Private subnet for internal resources that should not be directly accessible from the internet 
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.secure_vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "private-subnet"
+  }
 }
